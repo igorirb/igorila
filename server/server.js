12eced89bc55c572cac7f2233e30ed9e98be3260
igorirb <igor.irb@gmail.com>
2020-01-21 14:57:02 -0300

    structure changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const indexRoute = require('./routes/routes.js');
+const indexRoute = require('../routes/routes.js');
 
 // Instantiate express
 const app = express();
